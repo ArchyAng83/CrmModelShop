47404295872c66b9b6cb03aef4e19833c2af4a76
saltkeeper1@gmail.com <saltkeeper1@gmail.com>
2020-06-25 04:58:57 +0300

    Добавьте файлы проекта.

---
+++ CrmUi/CustomerForm.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace CrmUi
+{
+    partial class CustomerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.customerNameTextBox = new System.Windows.Forms.TextBox();
+            this.customerInputButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Введите имя";
+            // 
+            // customerNameTextBox
+            // 
+            this.customerNameTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.customerNameTextBox.Location = new System.Drawing.Point(93, 10);
+            this.customerNameTextBox.Name = "customerNameTextBox";
+            this.customerNameTextBox.Size = new System.Drawing.Size(193, 22);
+            this.customerNameTextBox.TabIndex = 1;
+            // 
+            // customerInputButton
+            // 
+            this.customerInputButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.customerInputButton.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.customerInputButton.Location = new System.Drawing.Point(207, 217);
+            this.customerInputButton.Name = "customerInputButton";
+            this.customerInputButton.Size = new System.Drawing.Size(75, 23);
+            this.customerInputButton.TabIndex = 2;
+            this.customerInputButton.Text = "button1";
+            this.customerInputButton.UseVisualStyleBackColor = true;
+            this.customerInputButton.Click += new System.EventHandler(this.customerInputButton_Click);
+            // 
+            // CustomerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(294, 252);
+            this.Controls.Add(this.customerInputButton);
+            this.Controls.Add(this.customerNameTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "CustomerForm";
+            this.Text = "CustomerForm";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox customerNameTextBox;
+        private System.Windows.Forms.Button customerInputButton;
+    }
+}
